c73293046becf5dd88c75be5bef2fa1ef573994d
Kyle Pamintuan <kylepamintuan@gmail.com>
2018-03-13 14:09:49 -0700

    Generate Access code increment working

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -16,9 +16,12 @@ export class HomePage
     lastAccessCode_ref: any;
     accessCode: any;
 
+
     constructor(public navCtrl: NavController, public afdb: AngularFireDatabase,
         public afAuth: AngularFireAuth, public alertCtrl: AlertController)
     {
+
+
         this.generateAccessCode();
 
         //this.accessCode = "123456";
@@ -27,25 +30,50 @@ export class HomePage
 
     generateAccessCode()
     {
-        this.lastAccessCode_ref = this.afdb.object('lastAccessCode');
-        this.lastAccessCode_ref.snapshotChanges().subscribe(action =>
-        {
-            // console.log(action.type);
-            // console.log(action.key)
-            console.log(action.payload.val())
+
+        // let accessCode: number
+        // this.lastAccessCode_ref = this.afdb.object('lastAccessCode')
+
+        // this.lastAccessCode_ref.valueChanges().subscribe(data =>
+        //     {
+
+        //          this.accessCode = data.value
+        //          console.log(this.accessCode)
+        //     });
+        this.lastAccessCode_ref = this.afdb.database.ref('lastAccessCode/value');
+
+        this.lastAccessCode_ref.transaction(function(value) {
+            console.log(value)
+            return value = value+1
+
         });
 
-        console.log('Last Access Code: ', this.lastAccessCode_ref);
+        
+    }
+
+
+    accessChatroom()
+    {
+        if (this.accessCode == this.lastAccessCode_ref)
+        {
+            //this.navCtrl.setRoot(ChatroomPage);
+        }
+        else
+        {
+            let alert = this.alertCtrl.create
+            (({
+                title: 'Access Code Invalid',
+                subTitle: 'error',
+                buttons: ['Dismiss']
+            }));
+            alert.present();
+            this.accessCode = '';
+        }
     }
 
     signOut(): void
     {
-    this.afAuth.auth.signOut();
-    this.navCtrl.push(LandingPage);
-    }
-
-    accessChatroom()
-    {
-
+       this.afAuth.auth.signOut();
+       this.navCtrl.push(LandingPage);
     }
 }
