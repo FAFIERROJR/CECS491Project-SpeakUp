ffcf079ac6d73b2ed9a4e708207e79aba96c6f63
Kyle Pamintuan <kylepamintuan@gmail.com>
2018-03-13 13:22:46 -0700

    access code function and access code feature

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,24 +1,51 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { AngularFireDatabase } from 'angularfire2/database';
 import { AngularFireAuth } from 'angularfire2/auth';
+import { AlertController } from 'ionic-angular';
 import { LandingPage } from '../landing/landing';
 
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
+    selector: 'page-home',
+    templateUrl: 'home.html'
 })
-export class HomePage {
 
-  constructor(public navCtrl: NavController, public afAuth: AngularFireAuth) {
+export class HomePage
+{
+    // chatroom_db_ref: any;
+    lastAccessCode_ref: any;
+    accessCode: any;
 
-  }
+    constructor(public navCtrl: NavController, public afdb: AngularFireDatabase,
+        public afAuth: AngularFireAuth, public alertCtrl: AlertController)
+    {
+        this.generateAccessCode();
 
-  /**
-   * signout
-   */
-  signOut(): void {
+        //this.accessCode = "123456";
+        //this.accessChatroom();
+    }
+
+    generateAccessCode()
+    {
+        this.lastAccessCode_ref = this.afdb.object('lastAccessCode');
+        this.lastAccessCode_ref.snapshotChanges().subscribe(action =>
+        {
+            // console.log(action.type);
+            // console.log(action.key)
+            console.log(action.payload.val())
+        });
+
+        console.log('Last Access Code: ', this.lastAccessCode_ref);
+    }
+
+    signOut(): void
+    {
     this.afAuth.auth.signOut();
     this.navCtrl.push(LandingPage);
-  }
+    }
 
+    accessChatroom()
+    {
+
+    }
 }
