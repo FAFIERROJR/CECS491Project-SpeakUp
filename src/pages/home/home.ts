350f36232ab77d3469ec365e751b0d14053f3a51
Francisco Fierro <f.a.fierrojr@gmail.com>
2018-03-12 19:06:40 -0700

    Sample chatroom card working

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -3,14 +3,20 @@ import { NavController } from 'ionic-angular';
 import { AngularFireAuth } from 'angularfire2/auth';
 import { LandingPage } from '../landing/landing';
 
+import { ChatroomcardsComponent } from '../../components/chatroomcards/chatroomcards';
+import { Observable } from '@firebase/util';
+import { AngularFireDatabase } from 'angularfire2/database';
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  courses: Observable<any[]>;
+  uid: string;
 
-  constructor(public navCtrl: NavController, public afAuth: AngularFireAuth) {
-
+  constructor(public navCtrl: NavController, public afAuth: AngularFireAuth, public afdb: AngularFireDatabase) {
+    this.uid = this.afAuth.auth.currentUser.uid;
+    this.courses = this.afdb.list('userProfile/' + this.uid + '/courses').valueChanges();
   }
 
   /**
