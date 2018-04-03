e4e11fff4c5055062271f833d9823cfe3d049870
DESKTOP-R1JT5CA\Danny <pthitb@yahoo.com>
2018-04-03 15:12:45 -0700

    Comment

--- src/components/components.module.ts
+++ src/components/components.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { ChatroomcardsComponent } from './chatroomcards/chatroomcards';
+import { CommentComponent } from './comment/comment';
 @NgModule({
-	declarations: [ChatroomcardsComponent],
+	declarations: [ChatroomcardsComponent,
+    CommentComponent],
 	imports: [],
-	exports: [ChatroomcardsComponent]
+	exports: [ChatroomcardsComponent,
+    CommentComponent]
 })
 export class ComponentsModule {}
