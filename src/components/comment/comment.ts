c228cf435d1588915fa6741c7d4c32a75bf93140
Matthew Le <matthew@dhcp-39-47-6.dhcpint.csulb.edu>
2018-04-10 14:45:49 -0700

    merged with backend

--- src/components/comment/comment.ts
+++ src/components/comment/comment.ts
@@ -3,6 +3,8 @@ import { AngularFireDatabase } from 'angularfire2/database';
 import { Comment } from '../../app/models/comment';
 import { Input } from '@angular/core';
 import { Chatroom } from '../../app/models/chatroom';
+import { CommentProvider } from '../../providers/commentprovider/commentprovider';
+import { Observable } from 'rxjs/Observable';
 
 
 
@@ -18,50 +20,58 @@ import { Chatroom } from '../../app/models/chatroom';
   templateUrl: 'comment.html'
 })
 export class CommentComponent {
-  @Input() roomPath: string;
-  @Input() commentID: any;
+  @Input('chatroom_id') chatroom_id: string;
+  @Input('comment_id') comment_id: string;
   text: string;
   commentRef: any;
-  comment: Comment;
+  comment: Observable<any>;
   room: Chatroom;
 
-  constructor(public afdb: AngularFireDatabase) {
-    this.commentRef = this.afdb.object('chatroom'+ this.roomPath + '/comments')
+  constructor(public commentProvider: CommentProvider) {
+    //this.commentRef = this.afdb.object('chatroom'+ this.roomPath + '/comments')
+    this.comment = commentProvider.getComment(this.chatroom_id, this.comment_id);
+    console.log("comment obvs", this.comment);
   }
 
-  createComment(){
+  ngOnInit(){
+    //this.commentRef = this.afdb.object('chatroom'+ this.roomPath + '/comments')
+    this.comment = this.commentProvider.getComment(this.chatroom_id, this.comment_id);
+    console.log("comment obvs, comment_id, chatroom_id", this.comment, this.comment_id, this.chatroom_id);
+  }
 
-    this.commentRef.push({
-      [this.comment.comment_id] : this.comment
+  // createComment(){
+
+  //   this.commentRef.push({
+  //     [this.comment.comment_id] : this.comment
 
     
-    });
-  }
+  //   });
+  // }
 
-  readComment(){
-    this.commentRef = this.afdb.database.ref('chatroom'+ this.roomPath + '/comments'+this.commentID)
+  //readComment(){
+    // this.commentRef = this.afdb.database.ref('chatroom'+ this.roomPath + '/comments'+this.commentID)
 
-    this.commentRef.transaction( value => 
-      {
+    // this.commentRef.transaction( value => 
+    //   {
 
-      this.comment.content = value;
-      return value;
+    //   this.comment.content = value;
+    //   return value;
 
     
-      }
-  );
+    //   }
+  //);
 
 
-  }
+  //}
 
   updateComment(){
-
 
   }
 
   deleteComment(){
-    this.commentRef = this.afdb.database.ref('chatroom'+ this.roomPath + '/comments'+this.commentID)
-    this.commentRef.remove(this.commentID);
+    // this.commentRef = this.afdb.database.ref('chatroom'+ this.roomPath + '/comments'+this.commentID)
+    // this.commentRef.remove(this.commentID);
+    this.commentProvider.deleteComment(this.chatroom_id, this.comment_id);
 
 
   }
--- src/components/commentslist/commentslist.ts
+++ src/components/commentslist/commentslist.ts
@@ -1,8 +1,9 @@
-import { Component, Injectable } from '@angular/core';
+import { Component, Injectable, Input } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
 import { AngularFireDatabase } from 'angularfire2/database';
 import { AlertController } from 'ionic-angular';
-
+import {CommentComponent} from '../comment/comment'
+import { CommentProvider } from '../../providers/commentprovider/commentprovider';
 /**
  * Generated class for the CommentslistComponent component.
  *
@@ -14,14 +15,24 @@ import { AlertController } from 'ionic-angular';
   templateUrl: 'commentslist.html'
 })
 export class CommentslistComponent {
+  //repushing
 
   comments: Observable<any[]>;
-  @Injectable() chatroom_id: string;
+  @Input('chatroom_id')
+    chatroom_id: string;
 
 
-  constructor(public afdb: AngularFireDatabase, public alertCtrl: AlertController) {
+  constructor(public commentProvider: CommentProvider, public alertCtrl: AlertController) {
     console.log('Hello CommentslistComponent Component');
-    this.comments = this.afdb.list('chatroom/' + this.chatroom_id + '/comments').valueChanges();
+    this.comments = this.commentProvider.getComments(this.chatroom_id);
+    console.log("chatroom_id", this.chatroom_id);
+    console.log("comments obvs", this.comments);
+  }
+
+  ngOnInit(){
+    this.comments = this.commentProvider.getComments(this.chatroom_id);
+    console.log("chatroom_id", this.chatroom_id);
+    console.log("comments obvs", this.comments);
   }
 
 
