c3762ca61af427d4ef4a4ca02c723ff7462e0738
Francisco Fierro <f.a.fierrojr@gmail.com>
2018-04-16 18:01:22 -0700

    created student list component and showStudentList() in chatroom

--- src/components/components.module.ts
+++ src/components/components.module.ts
@@ -3,15 +3,18 @@ import { ChatroomcardsComponent } from './chatroomcards/chatroomcards';
 import { CommentslistComponent } from './commentslist/commentslist';
 import { CoursepickerComponent } from './coursepicker/coursepicker';
 import { CommentComponent } from './comment/comment';
+import { StudentlistComponent } from './studentlist/studentlist';
 @NgModule({
 	declarations: [ChatroomcardsComponent,
     CommentslistComponent,
     CoursepickerComponent,
-    CommentComponent],
+    CommentComponent,
+    StudentlistComponent],
 	imports: [],
 	exports: [ChatroomcardsComponent,
     CommentslistComponent,
     CoursepickerComponent,
-    CommentComponent]
+    CommentComponent,
+    StudentlistComponent]
 })
 export class ComponentsModule {}
