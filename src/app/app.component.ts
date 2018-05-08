c8af4ab3deb654a8cdf7eec80f3a317f2e121154
Francisco Fierro <f.a.fierrojr@gmail.com>
2018-05-07 21:02:51 -0700

    set login page as root

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,7 +12,7 @@ import { IndexLandingPage } from '../pages/index-landing/index-landing';
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage:any = IndexLandingPage; //uncomment when building web
+  rootPage:any = LandingPage; //uncomment when building web
   //rootPage:any = LandingPage; //uncomment when building android or ios
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen, db: AngularFireDatabase) {
     platform.ready().then(() => {
